e0398e0f6902393ccabeadd6426bb270869c5911
sunhuatom <sunhuatom@gmail.com>
2017-06-17 13:51:15 +0800

    v1.0

---
+++ lasyman_setup_ss.sh
@@ -0,0 +1,138 @@
+#!/bin/bash
+##########################################
+# File Name: lasy_setup_ss.sh
+# 
+##########################################
+
+#----------------------------------------
+
+#check OS version
+CHECK_OS_VERSION=`cat /etc/issue |sed -n 1"$1"p|awk '{printf $1}' |tr 'a-z' 'A-Z'`
+
+#list the software need to be installed to the variable FILELIST
+UBUNTU_TOOLS_LIBS="python-pip"
+
+CENTOS_TOOLS_LIBS=""
+
+## check whether system is Ubuntu or not
+function check_OS_distributor(){
+	echo "checking distributor and release ID ..."
+	if [[ "${CHECK_OS_VERSION}" == "UBUNTU" ]] ;then
+		echo -e "\tCurrent OS: ${CHECK_OS_VERSION}"
+		UBUNTU=1
+	elif [[ "${CHECK_OS_VERSION}" == "CENTOS" ]] ;then
+		echo -e "\tCurrent OS: ${CHECK_OS_VERSION}!!!"
+		CENTOS=1
+	else
+		echo "not support ${CHECK_OS_VERSION} now"
+		exit 1
+	fi
+}
+
+## update system
+function update_system()
+{
+	if [[ ${UNUNTU} -eq 1 ]];then
+	{
+		echo "apt-get update"
+		apt-get update
+	}
+	elif [[ ${CENTOS} -eq 1 ]];then
+	{
+		##Webtatic EL6 for CentOS/RHEL 6.x
+		rpm -Uvh https://mirror.webtatic.com/yum/el6/latest.rpm
+		yum install mysql.`uname -i` yum-plugin-replace -y
+		yum replace mysql --replace-with mysql55w -y
+		yum replace php-common --replace-with=php55w-common -y
+	}
+	fi
+}
+
+
+#install one software every cycle
+function install_soft_for_each(){
+	echo "check OS version..."
+	check_OS_distributor
+	if [[ ${UBUNTU} -eq 1 ]];then
+		echo "Will install below software on your Ubuntu system:"
+		update_system
+		for file in ${UBUNTU_TOOLS_LIBS}
+		do
+			trap 'echo -e "\ninterrupted by user, exit";exit' INT
+			echo "========================="
+			echo "installing $file ..."
+			echo "-------------------------"
+			apt-get install $file -y
+			sleep 1
+			echo "$file installed ."
+		done
+		pip install shadowsocks
+
+	elif [[ ${CENTOS} -eq 1 ]];then
+		echo "Will install softwears on your CentOs system:"
+		update_system
+		for file in ${CENTOS_TOOLS_LIBS}
+		do
+			trap 'echo -e "\ninterrupted by user, exit";exit' INT
+			echo "========================="
+			echo "installing $file ..."
+			echo "-------------------------"
+			yum install $file -y
+			sleep 3
+			echo "$file installed ."
+		done
+		easy_install pip
+		pip install shadowsocks
+
+	else
+		echo "Other OS not support yet, please try Ubuntu or CentOs"
+		exit 1
+	fi
+}
+
+
+#start shadowsocks server
+function start_ss()
+{
+
+
+	cp shadowsocks.json /etc/shadowsocks.json
+	nohup sssever -c /etc/shadowsocks.json ‐d start
+
+	#add start-up
+	cp runssserver /etc/init.d/runssserver
+	chmod +x /etc/init.d/runssserver
+	cp runssserver.conf /etc/init/runssserver.conf
+	update-rc.d runssserver defaults 
+	# optimize shadowsocks
+	OPTI=local.conf
+	LOCALCONF=/etc/sysctl.d/local.conf
+	cat "$OPTI" >> "$LOCALCONF"
+	sysctl --system
+	#nofile limit
+	echo "root - nofile 16384" >> /etc/security/limits.conf
+	####
+	echo ""
+	echo "========================================================================e"
+	echo "congratulations, shadowsocks server starting..."
+	echo "========================================================================"
+	echo "The log file is in /var/log/shadowsocks.log..."
+	echo "========================================================================"
+}
+
+#====================
+# main
+#
+#judge whether root or not
+if [ "$UID" -eq 0 ];then
+read -p "(Please input root password):" ROOT_PASSWD
+if [ "$ROOT_PASSWD" = "" ]; then
+echo "Error: Password can't be NULL!!"
+exit 1
+fi
+	install_soft_for_each
+	start_ss
+else
+	echo -e "please run it as root user again !!!\n"
+	exit 1
+fi
